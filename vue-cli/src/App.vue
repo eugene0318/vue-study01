40f1c2b885e6c9a617983ab8c4b68c0749e66222
eugene <nttott@gmail.com>
2024-01-19 18:25:00 +0900

    props

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    {{ str }}
+    <!-- <app-header
+      v-bind:프롭스
+      속성
+      이름="상위 컴포넌트의 데이터 이름"
+    ></app-header> -->
+    <app-header v-bind:propsdata="str"></app-header>
+  </div>
+</template>
+
+<script>
+import AppHeader from "./components/AppHeader.vue";
+export default {
+  data: function () {
+    return {
+      str: "hiii",
+    };
+  },
+  components: {
+    "app-header": AppHeader,
+  },
+};
+</script>
+
+<style></style>
